96f1b2bb06d612f1d6355ce03a1be32759ec73ee
astro-israfil <153127727+astro-israfil@users.noreply.github.com>
2024-01-11 15:56:42 +0000

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+let score = false;
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// Number Conversion
+
+// "33abc" => NaN
+// true => 1
+// null => 0
+// undefined => 0
+// "33" => 33
+
+
+let isLoggedIn = 0
+
+// console.log(typeof isLoggedIn)
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+
+
+// Conversion to boolean
+// 0 => false; 1 => true
+// "" => false; "israfil" => true
+// null => false; undefined => false
+
+
+let someNumber = undefined
+
+let stringSomeNumber = String(someNumber)
+
+console.log(stringSomeNumber)
+console.log(typeof stringSomeNumber)
