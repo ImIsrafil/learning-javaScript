0d3df10d59327dd1ff7ef7b74006958594e674ce
astro-israfil <153127727+astro-israfil@users.noreply.github.com>
2024-01-14 15:19:56 +0000

    Basics of functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,30 @@
+function sayHi() {
+    console.log("Hello World!")
+}
+
+sayHi()
+
+function greeting(name) {
+    console.log(`Hey Programmer ${name}`)
+}
+
+greeting("Israfil")
+greeting()
+
+function addTwoNumbers(number1, number2) {
+    if (typeof number1 !== 'number' || typeof number2 !== 'number') {
+        return 0
+    }
+    return number1 + number2
+}
+
+console.log(addTwoNumbers(10, 30))
+console.log(addTwoNumbers())
+
+
+// Default Value of Parameters
+function anotherAddFunction(number1 = 0, number2 = 0) {
+    return number1 + number2
+}
+
+console.log(anotherAddFunction(10, 10))
